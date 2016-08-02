bf389f79431cb68d1fd82fb60c39daf2ee63ccf4
Elliot Bouvier <elliotbouvier@yahoo.fr>
2016-08-02 14:33:55 +0400

    twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render(':default:quiz.html.twig');
     }
 }
