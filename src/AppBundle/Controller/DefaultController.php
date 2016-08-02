5725c93eac03c411e8423a8f0f0d22e5e9292f50
Elliot Bouvier <elliotbouvier@yahoo.fr>
2016-08-02 14:49:02 +0400

    forms and views

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Quizz;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -11,8 +14,36 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->render(':default:quiz.html.twig');
+        $quizz = $this->getDoctrine()->getRepository(Quizz::class)->findAll();
+        return $this->render(':default:quiz.html.twig', array(
+            'questions' => $quizz,
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/new", name="add_quizz")
+     */
+    public function newAction(Request $request)
+    {
+        $quizz = new Quizz();
+        $form = $this->createFormBuilder($quizz)
+            ->add('Question', TextType::class)
+            ->add('Reponse', TextType::class)
+            ->add('Envoyer', SubmitType::class)
+            ->getForm();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($quizz);
+            $em->flush();
+            return $this->redirectToRoute('');
+        }
+        return $this->render(':default:addquiz.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
